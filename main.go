854831ddae156f69201951b0c59a4e36e51a5689
Bharadwajshivam28 <shivambharadwaj822@gmail.com>
2024-08-26 21:13:31 +0530

    feat/created publish command to Push Image on Dockerhub and cleanup images will be done on based of CLEANUP parameter in .enigma

--- main.go
+++ main.go
@@ -21,32 +21,25 @@ func loadDockerEnv() {
 func main() {
 	if len(os.Args) < 2 {
 		fmt.Println("Usage: enigma <command>")
-		fmt.Println("Commands: docker/run, docker/stop, docker/remove, docker/remove-image, bake")
+		fmt.Println("Commands: bake, publish")
 		os.Exit(1)
 	}
 
 	command := os.Args[1]
 
 	switch command {
-	case "docker/run":
-		loadDockerEnv()
-		docker.RunDockerContainer()
-	case "docker/stop":
-		loadDockerEnv()
-		docker.StopDockerContainer()
-	case "docker/remove":
-		loadDockerEnv()
-		docker.RemoveDockerContainer()
-	case "docker/remove-image":
-		loadDockerEnv()
-		docker.RemoveDockerImage()
+		
 	case "bake":
 		loadDockerEnv()
 		docker.BuildDockerImage()
 		docker.ScanDockerImage()
+	case "publish":
+		loadDockerEnv()
+		docker.TagDockerImage()
+		docker.PushDockerImage()
 		
 	default:
 		fmt.Println("Unknown command:", command)
-		fmt.Println("Commands: docker/run, docker/stop, docker/remove, docker/remove-image, bake")
+		fmt.Println("Commands: bake, publish")
 	}
 }
