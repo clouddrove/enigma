a45efa462b6469f23dd1abb7e131ac8a2cfb81e0
Bharadwajshivam28 <shivambharadwaj822@gmail.com>
2024-08-27 00:42:49 +0530

    feat:Publish for aws and gcp also ready

--- modules/docker/docker.go
+++ modules/docker/docker.go
@@ -1,72 +1,79 @@
 package docker
 
 import (
-	"fmt"
-	"log"
-	"os"
-	"os/exec"
+    "fmt"
+    "log"
+    "os"
+    "os/exec"
 )
 
 // BuildDockerImage builds a Docker image based on the environment variables.
 // It uses the `docker build` command to create the image with a specified tag.
 func BuildDockerImage() {
-	dockerImage := os.Getenv("DOCKER_IMAGE")
-	dockerTag := os.Getenv("DOCKER_TAG")
-	dockerImageName := fmt.Sprintf("%s:%s", dockerImage, dockerTag)
+    dockerImage := os.Getenv("DOCKER_IMAGE")
+    dockerTag := os.Getenv("DOCKER_TAG")
 
-	cmd := exec.Command("docker", "build", "-t", dockerImageName, ".")
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
+    if dockerImage == "" || dockerTag == "" {
+        log.Fatalf("DOCKER_IMAGE or DOCKER_TAG environment variable is not set")
+    }
 
-	fmt.Println("Building Docker image:", dockerImageName)
+    // Build the image using the base name (e.g., "aws")
+    cmd := exec.Command("docker", "build", "-t", dockerImage, ".")
+    cmd.Stdout = os.Stdout
+    cmd.Stderr = os.Stderr
 
-	err := cmd.Run()
-	if err != nil {
-		log.Fatalf("Error running docker build: %v", err)
-	}
+    fmt.Println("Building Docker image:", dockerImage)
 
-	fmt.Println("Build and Tag complete.")
+    err := cmd.Run()
+    if err != nil {
+        log.Fatalf("Error running docker build: %v", err)
+    }
+
+    fmt.Println("Build complete.")
+
+    TagDockerImage()
 }
 
 // ScanDockerImage performs a security scan of the Docker image and saves the report in SARIF format.
 // It uses the `docker scout` command to scan the image for vulnerabilities.
 func ScanDockerImage() {
-	dockerImage := os.Getenv("DOCKER_IMAGE")
-	dockerTag := os.Getenv("DOCKER_TAG")
-	dockerImageName := fmt.Sprintf("%s:%s", dockerImage, dockerTag)
+    dockerTag := os.Getenv("DOCKER_TAG")
 
-	sarifFile := "sarif.output.json"
+    if dockerTag == "" {
+        log.Fatalf("DOCKER_TAG environment variable is not set")
+    }
 
-	cmd := exec.Command("docker", "scout", "cves", dockerImageName, "--output", sarifFile)
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
+    sarifFile := "sarif.output.json"
 
-	fmt.Println("Scanning Docker image:", dockerImageName)
+    cmd := exec.Command("docker", "scout", "cves", dockerTag, "--output", sarifFile)
+    cmd.Stdout = os.Stdout
+    cmd.Stderr = os.Stderr
 
-	err := cmd.Run()
-	if err != nil {
-		log.Fatalf("Error running docker scout scan: %v", err)
-	}
+    fmt.Println("Scanning Docker image:", dockerTag)
 
-	fmt.Printf("Scan complete. Report saved to %s\n", sarifFile)
+    err := cmd.Run()
+    if err != nil {
+        log.Fatalf("Error running docker scout scan: %v", err)
+    }
+
+    fmt.Printf("Scan complete. Report saved to %s\n", sarifFile)
 }
 
-// TagDockerImage tags a Docker image with a new tag for Docker Hub.
+// TagDockerImage tags the built Docker image for the specified registry.
 // It uses the `docker tag` command to apply the new tag to the image.
 func TagDockerImage() {
     dockerImage := os.Getenv("DOCKER_IMAGE")
     dockerTag := os.Getenv("DOCKER_TAG")
-    dockerHubUsername := os.Getenv("DOCKERHUB_USERNAME")
-    dockerHubRepo := os.Getenv("DOCKERHUB_REPO")
     
-    sourceImage := fmt.Sprintf("%s:%s", dockerImage, dockerTag)
-    targetImage := fmt.Sprintf("%s/%s:%s", dockerHubUsername, dockerHubRepo, dockerTag)
+    if dockerImage == "" || dockerTag == "" {
+        log.Fatalf("DOCKER_IMAGE or DOCKER_TAG environment variable is not set")
+    }
 
-    cmdTag := exec.Command("docker", "tag", sourceImage, targetImage)
+    cmdTag := exec.Command("docker", "tag", dockerImage, dockerTag)
     cmdTag.Stdout = os.Stdout
     cmdTag.Stderr = os.Stderr
 
-    fmt.Printf("Tagging Docker image: docker tag %s %s\n", sourceImage, targetImage)
+    fmt.Printf("Tagging Docker image: %s as %s\n", dockerImage, dockerTag)
 
     err := cmdTag.Run()
     if err != nil {
@@ -76,53 +83,39 @@ func TagDockerImage() {
     fmt.Println("Docker image tagged successfully.")
 }
 
-// PushDockerImage pushes the tagged Docker image to Docker Hub and optionally cleans up local images.
-// It uses the `docker push` command to upload the image to the specified Docker Hub repository.
+
+// PushDockerImage pushes the tagged Docker image to the specified registry and optionally cleans up local images.
+// It uses the `docker push` command to upload the image to the registry specified in DOCKER_TAG.
 // Cleanup is performed by default or when explicitly set to "true". It's only disabled when set to "false".
 func PushDockerImage() {
-    dockerImage := os.Getenv("DOCKER_IMAGE")
     dockerTag := os.Getenv("DOCKER_TAG")
-    dockerHubUsername := os.Getenv("DOCKERHUB_USERNAME")
-    dockerHubRepo := os.Getenv("DOCKERHUB_REPO")
     cleanup := os.Getenv("CLEANUP")
-
-    localImage := fmt.Sprintf("%s:%s", dockerImage, dockerTag)
-    dockerHubImage := fmt.Sprintf("%s/%s:%s", dockerHubUsername, dockerHubRepo, dockerTag)
-
-    cmdPush := exec.Command("docker", "push", dockerHubImage)
+    
+    if dockerTag == "" {
+        log.Fatalf("DOCKER_TAG environment variable is not set")
+    }
+    
+    cmdPush := exec.Command("docker", "push", dockerTag)
     cmdPush.Stdout = os.Stdout
     cmdPush.Stderr = os.Stderr
-
-    fmt.Printf("Pushing Docker image to Docker Hub: docker push %s\n", dockerHubImage)
-
+    fmt.Printf("Pushing Docker image: docker push %s\n", dockerTag)
     err := cmdPush.Run()
     if err != nil {
         log.Fatalf("Error pushing docker image: %v", err)
     }
-
-    fmt.Println("Docker image successfully pushed to Docker Hub.")
-
+    fmt.Println("Docker image successfully pushed to the specified registry.")
+    
     if cleanup != "false" {
-        fmt.Println("Cleanup is enabled. Removing local images.")
-
-        cmdRmHub := exec.Command("docker", "rmi", dockerHubImage)
-        cmdRmHub.Stdout = os.Stdout
-        cmdRmHub.Stderr = os.Stderr
-        fmt.Printf("Removing Docker Hub tagged image: docker rmi %s\n", dockerHubImage)
-        if err := cmdRmHub.Run(); err != nil {
-            log.Printf("Error removing Docker Hub tagged image: %v", err)
+        fmt.Println("Cleanup is enabled. Removing tagged image.")
+        cmdRm := exec.Command("docker", "rmi", dockerTag)
+        cmdRm.Stdout = os.Stdout
+        cmdRm.Stderr = os.Stderr
+        fmt.Printf("Removing tagged image: docker rmi %s\n", dockerTag)
+        if err := cmdRm.Run(); err != nil {
+            log.Printf("Error removing tagged image: %v", err)
         }
-
-        cmdRmLocal := exec.Command("docker", "rmi", localImage)
-        cmdRmLocal.Stdout = os.Stdout
-        cmdRmLocal.Stderr = os.Stderr
-        fmt.Printf("Removing originally built image: docker rmi %s\n", localImage)
-        if err := cmdRmLocal.Run(); err != nil {
-            log.Printf("Error removing originally built image: %v", err)
-        }
-
         fmt.Println("Cleanup complete.")
     } else {
-        fmt.Println("Cleanup is disabled. Local images will not be removed.")
+        fmt.Println("Cleanup is disabled. Tagged image will not be removed.")
     }
 }
--- modules/docker/readme.md
+++ modules/docker/readme.md
@@ -5,10 +5,8 @@
 
 2. Set your values in `.enigma` file. To set values in `.enigma` just pass the values. This is an sample-
 ```
-DOCKER_IMAGE=test
-DOCKER_TAG=v1
-DOCKERHUB_USERNAME=myusername
-DOCKERHUB_REPO=myrepo
+DOCKER_IMAGE=nginx
+DOCKER_TAG=xyz:v1
 CLEANUP=true
 ```
 
@@ -27,7 +25,7 @@ go build -o enigma main.go
   ./enigma bake
   ```
  
-- To Push Image to Registry and cleanup Image at end(Cleanup will be only done if in `.enigma` CLEANUP is set   true or by default it will take true):
+- To Push Image to Registry and cleanup Image at end(Cleanup will be only done if in `.enigma` CLEANUP is set true or by default it will take true):
   ```
   ./enigma publish
   ```
