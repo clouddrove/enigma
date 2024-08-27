afde9eceac2085beccfe066eaa4b1480229d22cd
Bharadwajshivam28 <shivambharadwaj822@gmail.com>
2024-08-27 20:17:13 +0530

    feat:custom workflow for enigma docker

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,5 @@
 FROM golang:1.23
-
 WORKDIR /go/src/app
-
 COPY . .
-
 RUN go build -o enigma main.go
-
 ENTRYPOINT ["/go/src/app/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,22 +1,10 @@
 #!/bin/sh
-
 set -e
 
-# Check required inputs
-if [ -z "$INPUT_COMMAND" ]; then
-  echo "Error: command is not set"
-  exit 1
-fi
+# Pass environment variables to the Go application
+export DOCKER_IMAGE
+export DOCKER_TAG
+export CLEANUP
 
-if [ -z "$INPUT_DOCKER_IMAGE" ]; then
-  echo "Error: docker_image is not set"
-  exit 1
-fi
-
-if [ -z "$INPUT_DOCKER_TAG" ]; then
-  echo "Error: docker_tag is not set"
-  exit 1
-fi
-
-# Pass the arguments to the enigma binary
-/go/src/app/enigma $INPUT_COMMAND --docker-image "$INPUT_DOCKER_IMAGE" --docker-tag "$INPUT_DOCKER_TAG" --cleanup "$INPUT_CLEANUP"
+# Execute the command passed to the Docker container
+exec "/go/src/app/enigma" "$@"
