59a4027f8981c303559ce79fb426c1c195f07a32
Bharadwajshivam28 <shivambharadwaj822@gmail.com>
2024-08-28 03:02:40 +0530

    modified entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,10 +13,6 @@ export AWS_REGION
 
 export DOCKER_CONFIG="/github/home/.docker"
 
-aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
-aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
-aws configure set region $AWS_REGION
-
 aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin 924144197303.dkr.ecr.us-east-1.amazonaws.com
 
 exec "/go/src/app/enigma" "$@"
