a50b42b1ba9ead61a61897f5ea63c0e630d5acc9
Bharadwajshivam28 <shivambharadwaj822@gmail.com>
2024-08-28 02:31:23 +0530

    feat:added aws inputs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 set -e
 
+echo "AWS_ACCESS_KEY_ID is set: ${AWS_ACCESS_KEY_ID:+true}"
+echo "AWS_SECRET_ACCESS_KEY is set: ${AWS_SECRET_ACCESS_KEY:+true}"
+echo "AWS_REGION is set: ${AWS_REGION:+true}"
+
 export DOCKER_IMAGE
 export DOCKER_TAG
 export CLEANUP
