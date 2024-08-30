679a177dba0b440147178c69a47eb1110d631861
Bharadwajshivam28 <shivambharadwaj822@gmail.com>
2024-08-30 23:42:37 +0530

    feat: Added docker part in entrypoint.sh and in action.yml

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,14 @@ set -e
 if [ "$PROVIDER" = "aws" ]; then
   aws ecr get-login-password --region "$AWS_REGION" | docker login --username AWS --password-stdin "$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com"
 
+# Check if provider is Docker Hub
+elif [ "$PROVIDER" = "dockerhub" ]; then
+  echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
+
 else
   echo "Please pass a valid provider"
+  exit 1
 fi
 
+# Execute the application
 exec "/go/src/app/enigma" "$@"
