c2020d683d5a3a5e143bf9f66d0d03000b20e059
Shivam Kumar <shivambharadwaj822@gmail.com>
2024-08-31 01:34:53 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,23 +1,24 @@
 #!/bin/sh
 set -e
 
+# Source the Google Cloud SDK path
+if [ -f "/google-cloud-sdk/path.bash.inc" ]; then
+    . "/google-cloud-sdk/path.bash.inc"
+fi
+
 # Check if provider is AWS
 if [ "$PROVIDER" = "aws" ]; then
   aws ecr get-login-password --region "$AWS_REGION" | docker login --username AWS --password-stdin "$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com"
-
 # Check if provider is GCP
 elif [ "$PROVIDER" = "gcp" ]; then
   echo "$GCLOUD_AUTH" | gcloud auth activate-service-account --key-file=-
   gcloud auth configure-docker
-
 # Check if provider is Docker Hub
 elif [ "$PROVIDER" = "dockerhub" ]; then
   echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
-
 # Check if provider is GitHub Container Registry
 elif [ "$PROVIDER" = "github" ]; then
   echo "$TOKEN" | docker login ghcr.io -u "$GITHUB_USERNAME" --password-stdin
-
 else
   echo "Please pass a valid provider"
   exit 1
