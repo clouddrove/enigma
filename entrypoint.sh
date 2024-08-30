a193e9eede9de2d7d57c0644c873182135cc91b0
Shivam Kumar <shivambharadwaj822@gmail.com>
2024-08-31 02:18:35 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,16 +4,19 @@ set -e
 # Check if provider is AWS
 if [ "$PROVIDER" = "aws" ]; then
   aws ecr get-login-password --region "$AWS_REGION" | docker login --username AWS --password-stdin "$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com"
+
 # Check if provider is GCP
-# elif [ "$PROVIDER" = "gcp" ]; then
-#   gcloud auth login --update-adc --quiet
-#   gcloud auth configure-docker
+elif [ "$PROVIDER" = "gcp" ]; then
+  gcloud auth configure-docker "us-central1-docker.pkg.dev" --quiet
+
 # Check if provider is Docker Hub
 elif [ "$PROVIDER" = "dockerhub" ]; then
   echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
+
 # Check if provider is GitHub Container Registry
 elif [ "$PROVIDER" = "github" ]; then
   echo "$TOKEN" | docker login ghcr.io -u "$GITHUB_USERNAME" --password-stdin
+
 else
   echo "Please pass a valid provider"
   exit 1
