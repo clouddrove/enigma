e2548488f5eb4c9878941d6204c0d560671ab872
Shivam Kumar <shivambharadwaj822@gmail.com>
2024-08-30 22:56:30 +0530

    feat: modified entrypoint.sh and release.yml

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,9 @@ set -e
 # Check if provider is AWS
 if [ "$PROVIDER" = "aws" ]; then
   aws ecr get-login-password --region "$AWS_REGION" | docker login --username AWS --password-stdin "$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com"
+
+else
+  echo "Please pass a valid provider"
 fi
 
 exec "/go/src/app/enigma" "$@"
