7e0d79a1d57d00ab6b1e5a18f9e6d56b2a2ccf11
Bharadwajshivam28 <shivambharadwaj822@gmail.com>
2024-09-02 23:30:57 +0530

    feat:modified loadDockerEnv code to skip loading .enigma when running in github actons

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 set -e
 
-# Check if .enigma file exists
-if [ ! -f ".enigma" ]; then
-  echo "Warning: .enigma file not found. Please pass your env variables in the .enigma file for local use cases."
-fi
-
 # Check if provider is AWS
 if [ "$PROVIDER" = "aws" ]; then
   aws ecr get-login-password --region "$AWS_REGION" | docker login --username AWS --password-stdin "$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com"
