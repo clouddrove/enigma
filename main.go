05f5a33e437032ec430338d8a8d763913725f6b9
Bharadwajshivam28 <shivambharadwaj822@gmail.com>
2024-09-19 18:12:14 +0530

    feat: Added logic to use enigma env file based on name

--- main.go
+++ main.go
@@ -1,67 +1,70 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "os"
+	"flag"
+	"fmt"
+	"log"
+	"os"
 
-    "github.com/clouddrove/enigma/generate"
-    "github.com/clouddrove/enigma/modules/docker"
-    "github.com/joho/godotenv"
+	"github.com/clouddrove/enigma/generate"
+	"github.com/clouddrove/enigma/modules/docker"
+	"github.com/joho/godotenv"
 )
 
-func loadDockerEnv() {
-    isCICD := os.Getenv("CI") != "" || os.Getenv("GITHUB_ACTIONS") != ""
-
-    if !isCICD {
-        err := godotenv.Load(".enigma")
-        if err != nil {
-            log.Fatalf("Error loading .enigma file: %v", err)
-        }
-    } else {
-        fmt.Println("Running in CI/CD environment; skipping .enigma file load.")
-    }
+func loadDockerEnv(enigmaFile string) {
+	isCICD := os.Getenv("CI") != "" || os.Getenv("GITHUB_ACTIONS") != ""
+	if !isCICD {
+		err := godotenv.Load(enigmaFile)
+		if err != nil {
+			log.Fatalf("Error loading %s file: %v", enigmaFile, err)
+		}
+	} else {
+		fmt.Println("Running in CI/CD environment; skipping .enigma file load.")
+	}
 }
 
 func main() {
-    if len(os.Args) < 2 {
-        fmt.Println("Usage: enigma <command>")
-        fmt.Println("Commands: init, bake, publish, build-publish")
-        os.Exit(1)
-    }
+	enigmaFile := flag.String("enigma", ".enigma", "Path to the .enigma file")
+	flag.Parse()
 
-    command := os.Args[1]
+	if len(flag.Args()) < 1 {
+		fmt.Println("Usage: enigma [-enigma=<file>] <command>")
+		fmt.Println("Commands: init, bake, publish, build-publish")
+		os.Exit(1)
+	}
 
-    switch command {
-    case "init":
-        err := generate.GenerateEnigmaFile("modules/docker", ".enigma")
-        if err != nil {
-            fmt.Printf("Error generating .enigma file: %v\n", err)
-            os.Exit(1)
-        }
-    case "bake":
-        loadDockerEnv()
-        docker.InstallBinfmt()
-        docker.BuildDockerImage()
-        docker.ScanDockerImage()
-    case "publish":
-        loadDockerEnv()
-        docker.InstallBinfmt()
-        docker.TagDockerImage()
-        docker.PushDockerImage()
-    case "build-publish":
-        docker.LoadEnvFromEnigma()
-        docker.InstallBinfmt()
-        docker.BuildDockerImage()
-        docker.ScanDockerImage()
-        if os.Getenv("PUBLISH") == "true" {
-            docker.TagDockerImage()
-            docker.PushDockerImage()
-        } else {
-            fmt.Println("Publish is set to false. Skipping publish step.")
-        }
-    default:
-        fmt.Println("Unknown command:", command)
-        fmt.Println("Commands: init, bake, publish, build-publish")
-    }
+	command := flag.Args()[0]
+
+	switch command {
+	case "init":
+		err := generate.GenerateEnigmaFile("modules/docker", *enigmaFile)
+		if err != nil {
+			fmt.Printf("Error generating %s file: %v\n", *enigmaFile, err)
+			os.Exit(1)
+		}
+	case "bake":
+		loadDockerEnv(*enigmaFile)
+		docker.InstallBinfmt()
+		docker.BuildDockerImage()
+		docker.ScanDockerImage()
+	case "publish":
+		loadDockerEnv(*enigmaFile)
+		docker.InstallBinfmt()
+		docker.TagDockerImage()
+		docker.PushDockerImage()
+	case "build-publish":
+		docker.LoadEnvFromEnigma(*enigmaFile)
+		docker.InstallBinfmt()
+		docker.BuildDockerImage()
+		docker.ScanDockerImage()
+		if os.Getenv("PUBLISH") == "true" {
+			docker.TagDockerImage()
+			docker.PushDockerImage()
+		} else {
+			fmt.Println("Publish is set to false. Skipping publish step.")
+		}
+	default:
+		fmt.Println("Unknown command:", command)
+		fmt.Println("Commands: init, bake, publish, build-publish")
+	}
 }
