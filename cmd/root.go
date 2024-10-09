379cda939fdf38a32c9cfe3f37bdf53899e5ad03
Kanishk Pachauri <itskanishkp.py@gmail.com>
2024-10-10 04:20:21 +0530

    fix: README and add enigma command description

--- cmd/root.go
+++ cmd/root.go
@@ -12,12 +12,29 @@ var rootCmd = &cobra.Command{
 	Short: "enigma - The comprehensive DevOps toolkit",
 	Long: `Enigma is a tool designed to simplify the DevOps lifecycle, offering a seamless way to manage tools environments, 
 	build, scan, and publish. Below is a quick guide to getting started with Enigma and using its core commands.`,
-	//PersistentPreRun: func(cmd *cobra.Command, args []string) {
-	//	// This will run before any subcommand
-	//	fmt.Printf("Using enigma file: %s\n", enigmaFile)
-	//},
+
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Executing root command")
+		fmt.Println(`
+Enigma is a tool designed to simplify the DevOps lifecycle, offering a seamless way to manage tools environments, 
+        build, scan, and publish. Below is a quick guide to getting started with Enigma and using its core commands.
+
+Usage:
+  enigma [flags]
+  enigma [command]
+
+Available Commands:
+  bake          To Bake the command
+  build-publish To build and publish
+  completion    Generate the autocompletion script for the specified shell
+  help          Help about any command
+  init          To init the command
+  publish       To publish
+
+Flags:
+      --enigmafile string   Path to the .enigma file (default ".enigma")
+  -h, --help                help for enigma
+
+Use "enigma [command] --help" for more information about a command.`)
 	},
 }
 
