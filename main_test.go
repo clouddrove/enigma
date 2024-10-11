abf39d8a1050552652418cbe82fdb34648cd6d8b
Sarthak <54149390+sarthaknimbalkar@users.noreply.github.com>
2024-10-11 15:05:51 +0530

    Create main_test.go

---
+++ main_test.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestMainFunction(t *testing.T) {
+	// Call the main function
+	main()
+	// Since main does not return any value, there are no assertions to make.
+	// You could test side effects if applicable.
+}
